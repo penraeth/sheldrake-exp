fa214fabf1cf82cc2bcf38c45f4e10e7156f5be2
penraeth <sp@aknis.com>
2016-07-07 14:55:02 -0600

    google analytics added

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -17,6 +17,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="/templates/penraeth/js/analytics.js" type="text/javascript"></script>
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
